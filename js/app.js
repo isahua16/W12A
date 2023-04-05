73050f5f55b91226ac6869ca5370985997ea55df
isahua16 <isaelhuard@gmail.com>
2023-04-04 20:24:50 -0600

    Adding comments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 function query_dog_api(event)
 {
+    //If there is already an image of a dog with the dog_image id, remove it
     if(document.querySelector(`#dog_image`))
     {
         document.querySelector(`#dog_image`).remove();
     }
-
+    //If there is an error message displayer with the err_msg id, remove it
     if(document.querySelector(`#err_msg`))
     {
         document.querySelector(`#err_msg`).remove();
     }
 
+    //Insert HTML to inform the user that their query is loading
     image_container.insertAdjacentHTML(`beforeend`, `<h3 id="load_msg">Loading...</h3>`)
-    
+    //Make the API request
     axios.request(
         {
             url: `https://dog.ceo/api/breeds/image/random`
@@ -21,6 +23,7 @@ function query_dog_api(event)
 
 function dog_image_success(res)
 {
+    //Remove the loading message and inject the dog image using the data from the API
     document.querySelector(`#load_msg`).remove();
     image_container.insertAdjacentHTML(`beforeend`, 
     `<img id="dog_image" width="400px" src="${res[`data`][`message`]}" alt="Image of a dog">`);
@@ -28,6 +31,7 @@ function dog_image_success(res)
 
 function dog_image_fail(err)
 {
+    //Remove the loading message and inject and error message
     document.querySelector(`#load_msg`).remove();
     image_container.insertAdjacentHTML(`beforeend`, `<h3 id="err_msg">Something went wrong. Please try again.</h3>`)
 }
